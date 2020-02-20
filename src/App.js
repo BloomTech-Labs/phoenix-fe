87a864c74e00cda90afcaaae19330ac18c0b1374
Amy Barba <Amybarba70@gmail.com>
2020-02-19 20:15:08 -0800

    GA setup route instructions commented out

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, {useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ReactGa from 'react-ga';
-
+import {
+  BrowserRouter as Router, 
+  Switch, 
+  Route
+} from "react-router-dom"
 function App() {
 
   useEffect(() => {
    ReactGa.initialize('UA-158783725-1')
 
-  ReactGa.pageview('/')
+  ReactGa.pageview(window.location.pathname + window.location.search)
+//pathway to connect the routes and search
 }, [])
   return (
     <div className="App">
       <header className="App-header">
+
+        {/* <Router>
+          <Switch>
+            <Route path="/one" > </Route>
+            <Route path="/two" > </Route>
+          </Switch>
+        </Router> */}
         Hello
       </header>
     </div>
