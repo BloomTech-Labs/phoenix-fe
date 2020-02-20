8780b31bb2ebdcbb941a4fe8419f5d67915932fa
Amy Barba <Amybarba70@gmail.com>
2020-02-19 19:55:09 -0800

    add GA, attched tracking nmbr UA-158783725-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactGa from 'react-ga';
 
 function App() {
+
+  useEffect(() => {
+   ReactGa.initialize('UA-158783725-1')
+
+  ReactGa.pageview('/')
+}, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hello
       </header>
     </div>
   );
