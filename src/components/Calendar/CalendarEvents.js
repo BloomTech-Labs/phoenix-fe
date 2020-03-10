e9613fae708002a8c88968232d066cbeb54f790b
CScori <49799016+CScori@users.noreply.github.com>
2020-03-09 20:20:16 -0700

    add axios and checked function on search ...works

--- src/components/Calendar/CalendarEvents.js
+++ src/components/Calendar/CalendarEvents.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+//change props display make better card
 const CalendarEvents = (props) => {
+    console.log('props', props)
     return (
         <div>
-        <h1>Event Name:</h1>   
+        <h1>Event Name:{props.event.first_name}</h1>   
         <p> Event summary / Description</p> 
         <p>Locaton</p>
         <p>start</p>
--- src/components/Calendar/CalendarSearch.js
+++ src/components/Calendar/CalendarSearch.js
@@ -9,31 +9,32 @@ const [result, setRes] = useState([])
 
 useEffect(()=> { 
         axios
-        .get(``)
+        .get(`https://reqres.in/api/users?page=2`)
         .then(res => {
-            console.log('card data', res)
-            const search = {res}.filter(item => {
-                return item.title.toLowerCase().includes(string.toLowerCase())
+            console.log('card data', res.data.data)
+            const search = res.data.data.filter(item => {
+                return item.first_name.toLowerCase().includes(string.toLowerCase())
         })
         setRes(search);})
         .catch(err => console.log('err', err))
     }, [string])
 
+    //in return change to what we want to filter
+
     const handleChange = e => {
         setString(e.target.value)
     }
 
-    const handleSubmit = (e, {setQuery})=> {
+    const handleSubmit = (e, {setRes})=> {
         e.preventDefault()
-        setQuery(e.target.value)
-        format()
+        setRes(e.target.value)
     }
 
     
     return (
         <div>
             <h3>Search for events</h3>
-            <form>
+            <form onSubmit={handleSubmit}>
             <input 
               className="searchInput"
               onChange={handleChange}
@@ -44,9 +45,13 @@ useEffect(()=> {
               />
              <button type='submit'>Submit</button>
          </form>
-      <CalendarEvents/>
+         {result.map(data => (
+             <CalendarEvents key={data.id} event={data}/>
+         ))}
+      
     </div>
     )
 }
+
 
 export default CalendarSearch
