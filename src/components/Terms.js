ae79496cb7dad6dcae7f41123e77d1b9264ab538
Nick Dailey <n.dailey000@icloud.com>
2020-03-10 19:18:17 -0500

    added modal to terms

--- src/components/Terms.js
+++ src/components/Terms.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useState }from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function Terms() {
-
+    const classes = useStyles();
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
   
+    const handleOpen = () => {
+        setOpen(true);
+      };
+    
+      const handleClose = () => {
+        setOpen(false);
+      };
+
     return (
       <div className="fullTerms">
+           <button type="button" onClick={handleOpen}>
+        Open Modal
+      </button>
+      <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={open}
+        onClose={handleClose}
+      >
        <h2>TERMS OF USE</h2>
        
 
@@ -290,7 +337,7 @@ United States</p>
 
 
 
-    
+</Modal>
       </div>
     );
   }
