648ab6656a2b9fd37a439a7ffc09413b59e8ac97
Nick Dailey <n.dailey000@icloud.com>
2020-03-10 21:31:28 -0500

    updated to production

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const axiosWithAuth = () => {
     return axios.create({
-        baseURL: 'https://phoenix-be-staging.herokuapp.com',
+        baseURL: 'https://phoenix-be-production.herokuapp.com',
         headers: {
             authorization: localStorage.getItem('token'),
             
