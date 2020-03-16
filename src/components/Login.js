3803368d7b6998ab7761818bfa6002b90e7515ff
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-03-15 20:44:10 -0500

    starter login component to test props.history.push

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+    <form>
+        <input
+        placeholder="username"
+        />
+        <input
+        placeholder="password"
+        />
+        <button>Submit</button>
+    </form>
+    )
+}
+
+export default Login;
