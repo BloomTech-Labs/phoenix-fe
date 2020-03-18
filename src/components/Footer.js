eb2be97e0b6a2b6fbff6955facc851b02e299565
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-03-18 00:07:45 -0500

    Footer + Styles, partially mobile responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,48 +1,54 @@
 import React from 'react';
+import Terms from './Terms.js';
+import '../styles/Footer.css';
 
 const Footer = () => {
 
     return (
         <footer>
         <section>
-            <div>
-                Your Account
-                <ul>
-                    <li>Sign up</li>
-                    <li>Log in</li>
-                    <li>Help</li>
-                </ul>
-            </div>
-            <div>
-                Discover
-                <ul>
-                    <li>Calendar</li>
-                    <li>Topics</li>
-                    <li>Cities</li>
-                </ul>
-            </div>
-            <div>
-                Phoenix
-                <ul>
-                    <li>About</li>
-                    <li>Phoenix Premium</li>
-                    <li>Apps</li>
-                    <li>Blog</li>
-                </ul>
-            </div>
-            <div>
-                Follow us
-                <ul>
-                    <li>Facebook</li>
-                    <li>Twitter</li>
-                    <li>YouTube</li>
-                    <li>Instagram</li>
-                </ul>
+            <div className="info">
+                <div>
+                    Your Account
+                    <ul>
+                        <li>Sign up</li>
+                        <li>Log in</li>
+                        <li>Help</li>
+                    </ul>
+                </div>
+                <div>
+                    Discover
+                    <ul>
+                        <li>Calendar</li>
+                        <li>Topics</li>
+                        <li>Cities</li>
+                    </ul>
+                </div>
+                <div>
+                    Phoenix
+                    <ul>
+                        <li>About</li>
+                        <li>Phoenix Premium</li>
+                        <li>Apps</li>
+                        <li>Blog</li>
+                    </ul>
+                </div>
+                <div>
+                    Follow us
+                    <ul>
+                        <li>Facebook</li>
+                        <li>Twitter</li>
+                        <li>YouTube</li>
+                        <li>Instagram</li>
+                    </ul>
+                </div>
             </div>
         </section>
-        <div>
-
-        </div>
+        <section className="terms-section">
+            <div>
+            <Terms />
+            </div>         
+        </section>
         </footer>
     )
 }
