5b3fe39f253d4f702b3eb9e090a687d70321390e
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-03-17 20:44:13 -0500

    starter footer

---
+++ src/components/Footer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const Footer = () => {
+
+    return (
+        <footer>
+        <section>
+            <div>
+                Your Account
+                <ul>
+                    <li>Sign up</li>
+                    <li>Log in</li>
+                    <li>Help</li>
+                </ul>
+            </div>
+            <div>
+                Discover
+                <ul>
+                    <li>Calendar</li>
+                    <li>Topics</li>
+                    <li>Cities</li>
+                </ul>
+            </div>
+            <div>
+                Phoenix
+                <ul>
+                    <li>About</li>
+                    <li>Phoenix Premium</li>
+                    <li>Apps</li>
+                    <li>Blog</li>
+                </ul>
+            </div>
+            <div>
+                Follow us
+                <ul>
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                    <li>YouTube</li>
+                    <li>Instagram</li>
+                </ul>
+            </div>
+        </section>
+        <div>
+
+        </div>
+        </footer>
+    )
+}
+
+export default Footer;
