60a4b7b90eb0a53cb9e2771b5ad6e1ef681a9238
Joshua Draper <josh.drapergame@gmail.com>
2020-03-18 22:31:39 -0600

    wrote specific tests for clicking buttons

--- cypress/integration/registration_spec.js
+++ cypress/integration/registration_spec.js
@@ -1,5 +1,16 @@
 describe('My First Test', function() {
-    it('Does not do much!', function() {
-      expect(true).to.equal(true)
+    it('finds the content "New Phoenix"', function(){
+      cy.visit('/')
+      cy.get('button').contains('Register').click()
+      cy.contains('New Phoenix')
+    })
+    it('gets inputs', function(){
+      cy.visit('/')
+      cy.get('button').contains('Register').click().get('form').within(() => {
+        cy.get('input')
+        cy.get('input[name="email"]')
+        cy.get('button').click()
+      })
+      cy.contains('Please fill out this field.')
     })
   })
