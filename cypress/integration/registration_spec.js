68d967f3b1e60919fd1d032ec032c16dadfbfc66
Joshua Draper <josh.drapergame@gmail.com>
2020-03-19 20:17:01 -0600

    pulled origin to push local.

--- cypress/integration/registration_spec.js
+++ cypress/integration/registration_spec.js
@@ -2,15 +2,20 @@ describe('My First Test', function() {
     it('finds the content "New Phoenix"', function(){
       cy.visit('/')
       cy.get('button').contains('Register').click()
-      cy.contains('New Phoenix')
+      cy.contains('Birds of a feather... Join to find Phoenix meets near you')
     })
     it('gets inputs', function(){
       cy.visit('/')
       cy.get('button').contains('Register').click().get('form').within(() => {
         cy.get('input')
-        cy.get('input[name="email"]')
         cy.get('button').click()
       })
-      cy.contains('Please fill out this field.')
+    })
+    it('attempts to register a user', function(){
+      cy.visit('/')
+      cy.get('button').contains('Register').click().get('form').within(() => {
+        cy.get('input')
+        cy.get('button').click()
+      })
     })
   })
