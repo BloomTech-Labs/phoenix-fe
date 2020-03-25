12c0101b28b6cebaf88e668fcfc2578a2fc8fe64
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-03-24 21:00:31 -0500

    calendar search route

--- src/components/routes.js
+++ src/components/routes.js
@@ -3,18 +3,22 @@ import { Route, Switch } from 'react-router-dom';
 import LandingPage from './LandingPage.js';
 import Login from './Login.js';
 import Calendar from './Cal2/Calen.js';
+import CalendarSearch from './Calendar/CalendarSearch.js';
 
 const routes = [
         <Switch>
-            <Route key={2} exact path='/'>
+            <Route key={0} exact path='/'>
                 <LandingPage />
             </Route>
-            <Route key={0} path='/login'>
+            <Route key={1} path='/login'>
                 <Login />
             </Route>,
-            <Route key={1} path='/events'>
+            <Route key={2} path='/events'>
                 <Calendar />
             </Route>,
+            <Route key={3} path='/eventsearch'>
+                <CalendarSearch />
+            </Route>
         </Switch>
     ]
 
