3441438b3009c099a16ba77daad05c6125f97d60
nantonacci <nicole.antonacci@gmail.com>
2020-03-30 19:52:25 -0700

    Changed login and register to validate email and password. Register routes to calendar.

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,124 +1,132 @@
 import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
-import  Modal  from '@material-ui/core/Modal';
+import Modal from '@material-ui/core/Modal';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import {axiosWithAuth} from '../utils/axiosWithAuth.js';
+import { axiosWithAuth } from '../utils/axiosWithAuth.js';
 import { ModalStyle, getModalStyle } from '../styles/ModalFormStyles.js';
-  
+
 const useStyles = ModalStyle;
 
-const Registration = (props) => {
-  const [ credentials, setCredentials ] = useState({
+const Registration = props => {
+  const [credentials, setCredentials] = useState({
     username: '',
     password: '',
-    name:'',
-    email:'',
-    age: '',    
-  })
+    name: '',
+    email: '',
+    age: ''
+  });
 
-  const handleChange = (event) => {
-      setCredentials({
-          ...credentials, [event.target.name]: event.target.value
-      });
-  }
+  const handleChange = event => {
+    setCredentials({
+      ...credentials,
+      [event.target.name]: event.target.value
+    });
+  };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async event => {
     event.preventDefault();
-    handleClose()
-    axiosWithAuth().post('/auth/register', credentials)
-    .then(res => {
-      console.log(res)
-      props.history.push('/login')
-    })
-    .catch(err => console.log('ERROR MESSAGE', err.response))
-  }
+    handleClose();
+    axiosWithAuth()
+      .post('/auth/register', credentials)
+      .then(res => {
+        console.log(res);
+        props.history.push('/events');
+      })
+      .catch(err => console.log('ERROR MESSAGE', err.response));
+  };
 
   const classes = useStyles();
   const [modalStyle] = useState(getModalStyle);
   const [open, setOpen] = useState(false);
-    
+
   const handleOpen = () => {
     setOpen(true);
   };
-    
+
   const handleClose = () => {
     setOpen(false);
   };
 
-  return ( 
+  return (
     <>
-    <Button variant="outlined" onClick={handleOpen}>Register</Button>
-    
-    <Modal
-      aria-labelledby="simple-modal-title"
-      aria-describedby="simple-modal-description"
-      open={open}
-      onClose={handleClose}
-    >
-      <div style={modalStyle} className={classes.paper}>
-        <h2 id="simple-modal-title">Register</h2>
-        <p id="simple-modal-description">
-          Birds of a feather... Join to find Phoenix meets near you
-        </p>
+      <Button variant="outlined" onClick={handleOpen}>
+        Register
+      </Button>
+
+      <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={open}
+        onClose={handleClose}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <h2 id="simple-modal-title">Register</h2>
+          <p id="simple-modal-description">
+            Birds of a feather... Join to find Phoenix meets near you
+          </p>
           <div>
             <form className={classes.root} onSubmit={handleSubmit}>
               <TextField
-              required
-              id="filled-required1"
-              name="username"
-              label="Username"
-              value={props.username} 
-              onChange={handleChange}
-              placeholder="Username"
-              variant="outlined" 
+                required
+                id="filled-required1"
+                name="username"
+                label="Username"
+                value={props.username}
+                onChange={handleChange}
+                placeholder="Username"
+                variant="outlined"
               />
               <TextField
-              required
-              id="filled-required2"
-              name="password"
-              label="Password"
-              value={props.password} 
-              onChange={handleChange}
-              placeholder="Must be 8 characters"
-              variant="outlined"
+                required
+                id="filled-required2"
+                name="password"
+                label="Password"
+                type="password"
+                minlength="8"
+                value={props.password}
+                onChange={handleChange}
+                placeholder="Must be 8 characters"
+                variant="outlined"
               />
               <TextField
-              required
-              id="filled-required3"
-              name="name"
-              label="Full Name"
-              value={props.name} 
-              onChange={handleChange}
-              placeholder='Letters are nice'
-              variant="outlined"
+                required
+                id="filled-required3"
+                name="name"
+                label="Full Name"
+                value={props.name}
+                onChange={handleChange}
+                placeholder="Letters are nice"
+                variant="outlined"
               />
               <TextField
-              required
-              id="filled-required4"
-              name="email"
-              label="Email"
-              value={props.email} 
-              onChange={handleChange}
-              placeholder="@ required"
-              variant="outlined"
+                required
+                id="filled-required4"
+                name="email"
+                label="Email"
+                type="email"
+                value={props.email}
+                onChange={handleChange}
+                placeholder="@ required"
+                variant="outlined"
               />
               <TextField
-              required
-              id="filled-required5"
-              name="age"
-              label="Age"
-              value={props.age} 
-              onChange={handleChange}
-              variant="outlined"
+                required
+                id="filled-required5"
+                name="age"
+                type="number"
+                label="Age"
+                value={props.age}
+                onChange={handleChange}
+                variant="outlined"
               />
               <button onSubmit={handleSubmit}>Register</button>
             </form>
           </div>
-      </div>
-    </Modal>
+        </div>
+      </Modal>
     </>
   );
-}
+};
 
 export default withRouter(Registration);
