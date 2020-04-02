733380884b2d7b412956b9e84f9c80d7be7acf6e
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-04-01 22:37:38 -0500

    refactored Terms into smaller components

---
+++ src/components/Terms/Section8.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+export default function() {
+    return (
+        <>
+            <div className="e-sign">
+            <h2>ELECTRONIC COMMUNICATIONS, TRANSACTIONS, AND SIGNATURES</h2>
+            <p>
+              Visiting the Site, sending us emails, and completing online forms
+              constitute electronic communications. You consent to receive
+              electronic communications, and you agree that all agreements,
+              notices, disclosures, and other communications we provide to you
+              electronically, via email and on the Site, satisfy any legal
+              requirement that such communication be in writing. YOU HEREBY
+              AGREE TO THE USE OF ELECTRONIC SIGNATURES, CONTRACTS, ORDERS, AND
+              OTHER RECORDS, AND TO ELECTRONIC DELIVERY OF NOTICES, POLICIES,
+              AND RECORDS OF TRANSACTIONS INITIATED OR COMPLETED BY US OR VIA
+              THE SITE. You hereby waive any rights or requirements under any
+              statutes, regulations, rules, ordinances, or other laws in any
+              jurisdiction which require an original signature or delivery or
+              retention of non-electronic records, or to payments or the
+              granting of credits by any means other than electronic means. 
+            </p>
+          </div>
+           
+          <div className="cali-users">
+            <h2>CALIFORNIA USERS AND RESIDENTS</h2>
+            <p>
+              If any complaint with us is not satisfactorily resolved, you can
+              contact the Complaint Assistance Unit of the Division of Consumer
+              Services of the California Department of Consumer Affairs in
+              writing at 1625 North Market Blvd., Suite N 112, Sacramento,
+              California 95834 or by telephone at (800) 952-5210 or (916)
+              445-1254.
+            </p>
+          </div>
+           
+          <div className="misc">
+            <h2>MISCELLANEOUS</h2>
+            <p>
+              These Terms of Use and any policies or operating rules posted by
+              us on the Site or in respect to the Site constitute the entire
+              agreement and understanding between you and us. Our failure to
+              exercise or enforce any right or provision of these Terms of Use
+              shall not operate as a waiver of such right or provision. These
+              Terms of Use operate to the fullest extent permissible by law. We
+              may assign any or all of our rights and obligations to others at
+              any time. We shall not be responsible or liable for any loss,
+              damage, delay, or failure to act caused by any cause beyond our
+              reasonable control. If any provision or part of a provision of
+              these Terms of Use is determined to be unlawful, void, or
+              unenforceable, that provision or part of the provision is deemed
+              severable from these Terms of Use and does not affect the validity
+              and enforceability of any remaining provisions. There is no joint
+              venture, partnership, employment or agency relationship created
+              between you and us as a result of these Terms of Use or use of the
+              Site. You agree that these Terms of Use will not be construed
+              against us by virtue of having drafted them. You hereby waive any
+              and all defenses you may have based on the electronic form of
+              these Terms of Use and the lack of signing by the parties hereto
+              to execute these Terms of Use. Any attendees under the age of 18
+              will be the responsibility of the registered user. All users of
+              Phoenix app are liable for any injuries at any Phoenix event.  
+            </p>
+          </div>
+           
+          <div className="contact">
+            <h2>CONTACT US </h2>
+            <p>
+              In order to resolve a complaint regarding the Site or to receive
+              further information regarding use of the Site, please contact us
+              at: 
+            </p>
+            <p>Phoenix App los angeles, CA 90001 United States</p>
+            <p>
+              Telephone: <span>*********</span>
+            </p>
+            <p>projectphoenixlabs@gmail.com</p>
+          </div>
+          <div>
+            <p>By checking the box you agree to the terms of service. </p>
+          </div>
+        </>
+    )
+}
