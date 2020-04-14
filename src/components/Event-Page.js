5972c134dbf7764d9025d1ee351cd4a8205d591a
nantonacci <nicole.antonacci@gmail.com>
2020-04-13 20:52:48 -0700

    Working on time conversions for Event-Page

--- src/components/Event-Page.js
+++ src/components/Event-Page.js
@@ -9,11 +9,13 @@ import axios from 'axios';
 const EventPage = (props) => {
   const [string, setString] = useState('');
   const [result, setRes] = useState([]);
-
+  const [time, setTime] = useState({
+    startTime: '',
+    endTime: '',
+    startDate: '',
+    endDate: '',
+  });
   const eventID = Number(props.match.params.id);
-
-  console.log('eventID', eventID);
-  console.log('props', props);
 
   useEffect(() => {
     axios
@@ -26,35 +28,58 @@ const EventPage = (props) => {
         });
 
         let event = await results.filter((item) => {
-          return item.event_id == eventID;
+          return item.event_id === eventID;
         })[0];
         setTimeout(console.log('event:', event), 2000);
 
-        console.log('res:', res);
-
         setRes(event);
+        console.log('taimu', event.end_time);
 
-        console.log('results:', results);
-
-        // for (let i = 0; i < results.length; i++) {
-        //   results[i].start_time = new Date(
-        //     results[i].start_date
-        //   ).toLocaleTimeString();
-        //   results[i].end_time = new Date(
-        //     results[i].end_date
-        //   ).toLocaleTimeString();
-        //   results[i].start_date = new Date(
-        //     results[i].start_date
-        //   ).toDateString();
-        // }
+        //convert time
+        let shorty = Number(event.end_time.substr(0, 2));
+        let shorty30 = Number(event.end_time.substr(4, 6));
+        let endT;
+        if (shorty > 12) {
+          endT = `${shorty - 12}:${shorty30} PM`;
+        }
+        if (shorty === 12) {
+          endT = `${shorty}: ${shorty30} PM`;
+        } else {
+          endT = `${shorty}: ${shorty30} AM`;
+        }
+        setTime({ endTime: endT });
+        // convert date
+        let start = event.start_date;
+        let startD = new Date(start);
+        let end = event.end_date;
+        let endD = new Date(end);
       })
       .then((_) => setString(''))
       .catch((err) => console.log('Problem retrieving events', 'Error: ', err));
+
+    // result.eventtime do something
+
+    if (result.start_time > '12:00:00') {
+      let startO = result.start_time - 12 + 'pm';
+      setTime({ startTime: startO });
+    }
+    // if time > 12, subtract 12 and add PM
+    // else add AM
+    // date rearrange
+    // save as new variable
   }, [eventID]);
 
   return (
     <>
-      <div>hey it's the event page</div>
+      <h2>{result.summary}</h2>
+      <p>{result.description}</p>
+      <p>Location: {result.location}</p>
+      <p>
+        Starts: {time.startDate} {time.startTime}
+      </p>
+      <p>
+        Ends: {time.endDate} {time.endTime}
+      </p>
     </>
   );
 };
