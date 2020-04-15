08aaa9d4e0f180c1aa4624bf76af1fb5954e1980
Nick Dailey <n.dailey000@icloud.com>
2020-04-14 21:17:12 -0500

    added button to link you from event to calendar

--- src/components/MockEvents.js
+++ src/components/MockEvents.js
@@ -6,7 +6,8 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import axios from 'axios';
-
+import Registration from "./Registration";
+import Button from '@material-ui/core/Button';
 
 
 export default function MockEvents(){ 
@@ -41,7 +42,15 @@ export default function MockEvents(){
         <ExpansionPanelDetails>
           <Typography>
            Whether you are new to wine tasting or beer sampling or a long time connoisseur, please join us for a fun filled night of connecting with new friends or reuniting with past friends sampling local varieties and enjoying a live local band. Click the "Register" button below.
+    
+           
+           <Button href="/events" >
+  More about the event here 
+  </Button>
           </Typography>
+          <div>
+         
+          </div>
         </ExpansionPanelDetails>
       </ExpansionPanel>
       <ExpansionPanel>
@@ -55,6 +64,9 @@ export default function MockEvents(){
         <ExpansionPanelDetails>
           <Typography>
           Whether you are new to marathons or running as a sport or a long time member, please join us for a fun filled night of connecting with new friends or reuniting with past friends sampling nutrition supplements, trying out new training equipment.Click the "Register" button below.
+          <Button href="/events" >
+  More about the event here 
+  </Button>
           </Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>
@@ -69,6 +81,9 @@ export default function MockEvents(){
         <ExpansionPanelDetails>
           <Typography>
           Whether you are new to attending Movie Premiers, Specialty Parks or Museums  or you are a long time fan, please join us for a fun filled night. Connect with new friends or reunite with past friends by watching new movie trailers, video tours of the best parks and museums while enjoying light  snacks and beverages. Click the "Register" button below.
+          <Button href="/events" >
+  More about the event here 
+  </Button>
           </Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>
