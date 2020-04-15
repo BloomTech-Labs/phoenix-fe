dac83a27cef117dab028acf7ee2017dd969deee1
Nick Dailey <n.dailey000@icloud.com>
2020-04-14 21:24:02 -0500

    finished button

--- src/components/MockEvents.js
+++ src/components/MockEvents.js
@@ -6,7 +6,6 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import axios from 'axios';
-import Registration from "./Registration";
 import Button from '@material-ui/core/Button';
 
 
@@ -41,10 +40,8 @@ export default function MockEvents(){
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <Typography>
-           Whether you are new to wine tasting or beer sampling or a long time connoisseur, please join us for a fun filled night of connecting with new friends or reuniting with past friends sampling local varieties and enjoying a live local band. Click the "Register" button below.
-    
-           
-           <Button href="/events" >
+           Whether you are new to wine tasting or beer sampling or a long time connoisseur, please join us for a fun filled night of connecting with new friends or reuniting with past friends sampling local varieties and enjoying a live local band. 
+           <Button  variant="outlined" href="/events" >
   More about the event here 
   </Button>
           </Typography>
@@ -63,8 +60,8 @@ export default function MockEvents(){
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <Typography>
-          Whether you are new to marathons or running as a sport or a long time member, please join us for a fun filled night of connecting with new friends or reuniting with past friends sampling nutrition supplements, trying out new training equipment.Click the "Register" button below.
-          <Button href="/events" >
+          Whether you are new to marathons or running as a sport or a long time member, please join us for a fun filled night of connecting with new friends or reuniting with past friends sampling nutrition supplements, trying out new training equipment.
+          <Button  variant="outlined" href="/events" >
   More about the event here 
   </Button>
           </Typography>
@@ -80,8 +77,8 @@ export default function MockEvents(){
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <Typography>
-          Whether you are new to attending Movie Premiers, Specialty Parks or Museums  or you are a long time fan, please join us for a fun filled night. Connect with new friends or reunite with past friends by watching new movie trailers, video tours of the best parks and museums while enjoying light  snacks and beverages. Click the "Register" button below.
-          <Button href="/events" >
+          Whether you are new to attending Movie Premiers, Specialty Parks or Museums  or you are a long time fan, please join us for a fun filled night. Connect with new friends or reunite with past friends by watching new movie trailers, video tours of the best parks and museums while enjoying light  snacks and beverages. 
+          <Button variant="outlined" href="/events" >
   More about the event here 
   </Button>
           </Typography>
