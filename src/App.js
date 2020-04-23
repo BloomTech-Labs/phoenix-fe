684d25d874dd48b0f3b62e5daf85b74f472661d4
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-04-23 10:32:20 -0500

    code climate

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { useEffect } from 'react';
-
 import './App.css';
-import ReactGa from 'react-ga';
+// import ReactGa from 'react-ga';
 import Navbar from './components/Navbar.js';
 import Footer from './components/Footer.js';
 import Routes from './components/routes.js';
 
 
 function App() {
-  useEffect(() => {
-    window.onload = function() {
-      ReactGa.set({ username: "Phoenix App" });
-      ReactGa.initialize('UA-158783725-1');
-      ReactGa.pageview(window.location.pathname + window.location.search);
-    }
-  }, []);
+  // useEffect(() => {
+
+  //     ReactGa.initialize('UA-158783725-1');
+  //     ReactGa.pageview(window.location.pathname + window.location.search);
+
+  // }, []);
   return (
     <div className="App">
 
--- src/App.test.js
+++ src/App.test.js
@@ -5,15 +5,6 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from './App.js';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <Router>
-        <App />
-    </Router>, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
 it("renders 3 'Phoenix' elements", () => {
   const div = document.createElement('div');
   const {  getAllByText } = render(
