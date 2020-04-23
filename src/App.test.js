0d8656721fffb97b3f87e7ca6ff4862957c8c026
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-04-23 09:13:02 -0500

    simple render test

---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App.js';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
