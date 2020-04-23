7b3d30c45891ec286bd283e41014985f07390e5b
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-04-23 09:41:18 -0500

    added second sample test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App.js';
 
 it('renders without crashing', () => {
@@ -10,4 +12,16 @@ it('renders without crashing', () => {
         <App />
     </Router>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders 3 'Phoenix' elements", () => {
+  const div = document.createElement('div');
+  const {  getAllByText } = render(
+    <Router>
+        <App />
+    </Router>, div);
+  ReactDOM.unmountComponentAtNode(div);
+
+  const mainContentTitle = getAllByText(/Phoenix/i);
+  expect(mainContentTitle).toHaveLength(3) ;
 });
