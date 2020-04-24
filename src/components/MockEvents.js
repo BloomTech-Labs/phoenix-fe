b71e0c8b8940ec8e79f4c065e039f4e69aaf9464
Amy Barba <Amybarba70@gmail.com>
2020-04-23 19:20:56 -0700

    syncing files for merge

--- src/components/MockEvents.js
+++ src/components/MockEvents.js
@@ -6,7 +6,6 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Button from '@material-ui/core/Button';
-import LandPageCarousel from "../components/LandPageCarousel";
 
 export default function MockEvents(){ 
   const useStyles = MockEventStyles;
