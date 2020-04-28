20dc03a177cde748c9384288654fcec58779ddd4
ian-sanchez <silkyhairbear@gmail.com>
2020-04-27 19:58:36 -0700

    login snapshot

--- src/__tests__/login.test.js
+++ src/__tests__/login.test.js
@@ -4,12 +4,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import renderer from 'react-test-renderer'
-import Registration from '../components/Registration.js';
+import Login from '../components/Login';
 
 it("Renders Registraion page to match snapshot", () => {
   const tree = renderer
 
-    .create(<Router><Registration /></Router>)
+    .create(<Router><Login /></Router>)
     .toJSON();
 
     console.log(tree, 'tree')
