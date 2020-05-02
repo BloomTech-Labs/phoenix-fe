d1b53c700620ba736defed6d86616159c24d7557
Amy Barba <Amybarba70@gmail.com>
2020-05-02 12:45:23 -0700

    uniform styling on login and register modal

--- src/components/Authorized/Login.js
+++ src/components/Authorized/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { ModalStyle, getModalStyle } from '../../styles/ModalFormStyles.js';
+import { Typography } from '@material-ui/core';
 import { withRouter } from 'react-router-dom';
 import Modal from '@material-ui/core/Modal';
 import TextField from '@material-ui/core/TextField';
@@ -9,91 +10,93 @@ import { axiosWithAuth } from '../../utils/axiosWithAuth.js';
 const useStyles = ModalStyle;
 
 const Login = props => {
-  const [credentials, setCredentials] = useState({
-    username: '',
-    password: ''
-  });
+const [credentials, setCredentials] = useState({
+username: '',
+password: ''
+});
 
-  const handleChange = event => {
-    setCredentials({
-      ...credentials,
-      [event.target.name]: event.target.value
-    });
-  };
+const handleChange = event => {
+setCredentials({
+...credentials,
+[event.target.name]: event.target.value
+});
+};
 
-  const handleSubmit = async event => {
-    event.preventDefault();
-    handleClose();
-    axiosWithAuth()
-      .post('/auth/login', credentials)
-      .then(res => {
-        console.log('DATA: ', res.data);
-        localStorage.setItem('token', res.data.token);
-        props.history.push('/dashboard');
-      })
-      .catch(err => console.log('ERROR MESSAGE', err.response));
-  };
+const handleSubmit = async event => {
+event.preventDefault();
+handleClose();
+axiosWithAuth()
+.post('/auth/login', credentials)
+.then(res => {
+console.log('DATA: ', res.data);
+localStorage.setItem('token', res.data.token);
+props.history.push('/dashboard');
+})
+.catch(err => console.log('ERROR MESSAGE', err.response));
+};
 
-  const classes = useStyles();
-  const [modalStyle] = useState(getModalStyle);
-  const [open, setOpen] = useState(false);
+const classes = useStyles();
+const [modalStyle] = useState(getModalStyle);
+const [open, setOpen] = useState(false);
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+const handleOpen = () => {
+setOpen(true);
+};
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-  return (
-    <>
-      <Button
-        variant="outlined"
-        style={{ marginLeft: '16px' }}
-        onClick={handleOpen}
-      >
-        Login
-      </Button>
-      <Modal
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-        open={open}
-        onClose={handleClose}
-      >
-        <div style={modalStyle} className={classes.paper}>
-          <h2 id="simple-modal-title">Log in</h2>
-          <p id="simple-modal-description">Log in to see what's out there</p>
-          <div>
-            <form className={classes.root} onSubmit={handleSubmit}>
-              <TextField
-                required
-                id="filled-required1"
-                name="username"
-                label="Username"
-                value={props.username}
-                onChange={handleChange}
-                placeholder="Username"
-                variant="outlined"
-              />
-              <TextField
-                required
-                id="filled-required2"
-                name="password"
-                label="Password"
-                type="password"
-                minlength="8"
-                value={props.password}
-                onChange={handleChange}
-                placeholder="Must be 8 characters"
-                variant="outlined"
-              />
-              <button onSubmit={handleSubmit}>Log in</button>
-            </form>
-          </div>
-        </div>
-      </Modal>
-    </>
-  );
+const handleClose = () => {
+setOpen(false);
+};
+return (
+<>
+<Button
+variant="outlined"
+style={{ marginLeft: '16px' }}
+onClick={handleOpen}
+>
+Login
+</Button>
+<Modal
+aria-labelledby="simple-modal-title"
+aria-describedby="simple-modal-description"
+open={open}
+onClose={handleClose}
+>
+<div style={modalStyle} className={classes.paper}>
+<Typography id="simple-modal-title" style={{ fontSize: '2rem'}}>Log In</Typography>
+<Typography id="simple-modal-description" style={{ fontSize: '1rem'}}>
+Log in to see what's out there
+</Typography>
+<div>
+<form className={classes.root} onSubmit={handleSubmit}>
+<TextField
+required
+id="filled-required1"
+name="username"
+label="Username"
+value={props.username}
+onChange={handleChange}
+placeholder="Username"
+variant="outlined"
+/>
+<TextField
+required
+id="filled-required2"
+name="password"
+label="Password"
+type="password"
+minlength="8"
+value={props.password}
+onChange={handleChange}
+placeholder="Must be 8 characters"
+variant="outlined"
+/>
+<Button variant="outlined" onSubmit={handleSubmit}>Log In</Button>
+</form>
+</div>
+</div>
+</Modal>
+</>
+);
 };
 
 export default withRouter(Login);
--- src/components/Authorized/Registration.js
+++ src/components/Authorized/Registration.js
@@ -5,128 +5,131 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { axiosWithAuth } from '../../utils/axiosWithAuth.js';
 import { ModalStyle, getModalStyle } from '../../styles/ModalFormStyles.js';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
 
 const useStyles = ModalStyle;
 
 const Registration = props => {
-  const [credentials, setCredentials] = useState({
-    username: '',
-    password: '',
-    name: '',
-    email: '',
-    age: ''
-  });
 
-  const handleChange = event => {
-    setCredentials({
-      ...credentials,
-      [event.target.name]: event.target.value
-    });
-  };
+const [credentials, setCredentials] = useState({
+username: '',
+password: '',
+name: '',
+email: '',
+age: ''
+});
 
-  const handleSubmit = async event => {
-    event.preventDefault();
-    handleClose();
-    axiosWithAuth()
-      .post('/auth/register', credentials)
-      .then(res => {
-        console.log(res);
-        props.history.push('/dashboard');
-      })
-      .catch(err => console.log('ERROR MESSAGE', err.response));
-  };
+const handleChange = event => {
+setCredentials({
+...credentials,
+[event.target.name]: event.target.value
+});
+};
 
-  const classes = useStyles();
-  const [modalStyle] = useState(getModalStyle);
-  const [open, setOpen] = useState(false);
+const handleSubmit = async event => {
+event.preventDefault();
+handleClose();
+axiosWithAuth()
+.post('/auth/register', credentials)
+.then(res => {
+console.log(res);
+props.history.push('/dashboard');
+})
+.catch(err => console.log('ERROR MESSAGE', err.response));
+};
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+const classes = useStyles();
+const [modalStyle] = useState(getModalStyle);
+const [open, setOpen] = useState(false);
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+const handleOpen = () => {
+setOpen(true);
+};
 
-  return (
-    <>
-      <Button variant="outlined" onClick={handleOpen}>
-        {props.title}
-      </Button>
+const handleClose = () => {
+setOpen(false);
+};
 
-      <Modal
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-        open={open}
-        onClose={handleClose}
-      >
-        <div style={modalStyle} className={classes.paper}>
-          <h2 id="simple-modal-title">Register</h2>
-          <p id="simple-modal-description">
-            Birds of a feather... Join to find Phoenix meets near you
-          </p>
-          <div>
-            <form className={classes.root} onSubmit={handleSubmit}>
-              <TextField
-                required
-                id="filled-required1"
-                name="username"
-                label="Username"
-                value={props.username}
-                onChange={handleChange}
-                placeholder="Username"
-                variant="outlined"
-              />
-              <TextField
-                required
-                id="filled-required2"
-                name="password"
-                label="Password"
-                type="password"
-                minlength="8"
-                value={props.password}
-                onChange={handleChange}
-                placeholder="Must be 8 characters"
-                variant="outlined"
-              />
-              <TextField
-                required
-                id="filled-required3"
-                name="name"
-                label="Full Name"
-                value={props.name}
-                onChange={handleChange}
-                placeholder="Letters are nice"
-                variant="outlined"
-              />
-              <TextField
-                required
-                id="filled-required4"
-                name="email"
-                label="Email"
-                type="email"
-                value={props.email}
-                onChange={handleChange}
-                placeholder="@ required"
-                variant="outlined"
-              />
-              <TextField
-                required
-                id="filled-required5"
-                name="age"
-                type="number"
-                label="Age"
-                value={props.age}
-                onChange={handleChange}
-                variant="outlined"
-              />
-              <button onSubmit={handleSubmit}>Register</button>
-            </form>
-          </div>
-        </div>
-      </Modal>
-    </>
-  );
+return (
+<>
+<Button variant="outlined" onClick={handleOpen}>
+{props.title}
+</Button>
+
+<Modal
+aria-labelledby="simple-modal-title"
+aria-describedby="simple-modal-description"
+open={open}
+onClose={handleClose}
+>
+<div style={modalStyle} className={classes.paper}>
+<Typography id="simple-modal-title" style={{ fontSize: '2rem'}}>Register</Typography>
+<Typography id="simple-modal-description" style={{ fontSize: '1rem'}}>
+Birds of a feather... Join to find Phoenix meets near you
+</Typography>
+<div>
+<form className={classes.root} onSubmit={handleSubmit}>
+<TextField
+required
+id="filled-required1"
+name="username"
+label="Username"
+value={props.username}
+onChange={handleChange}
+placeholder="Username"
+variant="outlined"
+/>
+<TextField
+required
+id="filled-required2"
+name="password"
+label="Password"
+type="password"
+minlength="8"
+value={props.password}
+onChange={handleChange}
+placeholder="Must be 8 characters"
+variant="outlined"
+/>
+<TextField
+required
+id="filled-required3"
+name="name"
+label="Full Name"
+value={props.name}
+onChange={handleChange}
+placeholder="Letters are nice"
+variant="outlined"
+/>
+<TextField
+required
+id="filled-required4"
+name="email"
+label="Email"
+type="email"
+value={props.email}
+onChange={handleChange}
+placeholder="@ required"
+variant="outlined"
+/>
+<TextField
+required
+id="filled-required5"
+name="age"
+type="number"
+label="Age"
+value={props.age}
+onChange={handleChange}
+variant="outlined"
+/>
+<Button variant="outlined" onSubmit={handleSubmit}>Register</Button>
+</form>
+</div>
+</div>
+</Modal>
+</>
+);
 };
 
 export default withRouter(Registration);
