5c35a114b3daf2923f80e3bd4ac138db8d62af06
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-05-02 15:14:40 -0500

    icon spacing

--- src/styles/NavbarStyles.js
+++ src/styles/NavbarStyles.js
@@ -31,7 +31,7 @@ const NavbarStyle = makeStyles(theme => ({
       },
     },
     searchIcon: {
-      // padding: theme.spacing(0, 2),
+      padding: theme.spacing(0, 2),
       height: '100%',
       position: 'absolute',
       pointerEvents: 'none',
