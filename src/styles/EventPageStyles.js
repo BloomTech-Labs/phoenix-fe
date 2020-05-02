c111b5c1addca3f4f2b2dbd48cb8bc4632bc41ce
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-05-02 17:50:27 -0500

    styled the event page

---
+++ src/styles/EventPageStyles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const EventPageStyle = makeStyles(theme => ({
+    root: {
+        flexGrow: 10,
+        margin: "auto",
+    },
+    card: {
+        padding: "1rem"
+    }
+
+
+}))
+
+export default EventPageStyle;
